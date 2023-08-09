486e60ef19641cc628e8bac3b763561e0a58c624
Bregy Malpartida <bregy@minsky.cc>
2023-08-09 10:38:54 -0500

    feat: updated chat completion

--- self/interface.go
+++ self/interface.go
@@ -5,18 +5,36 @@ import (
 	"database/sql"
 	"fmt"
 
+	"github.com/google/uuid"
 	"github.com/minskylab/brain/channels"
+	"github.com/minskylab/brain/models"
+	"github.com/pkg/errors"
+	"github.com/sashabaranov/go-openai"
 )
 
 func (brain *BrainEngine) GenerateConversationResponse(ctx context.Context, channel channels.ChannelType, sender string, message string) (string, error) {
-	fmt.Println("Message from", sender, ":\n", message)
+	var user models.User
+	var err error
 
 	if channel == channels.WhatsAppChannel {
-		brain.DatabaseClient.GetUserByJID(ctx, sql.NullString{
+		user, err = brain.DatabaseClient.GetUserByJID(ctx, sql.NullString{
 			String: sender,
 			Valid:  true,
 		})
+		if err != nil {
+			return "", errors.WithStack(err)
+		}
+	} else if channel == channels.TelegramChannel {
+		user, err = brain.DatabaseClient.GetUserByTelegramID(ctx, sql.NullString{
+			String: sender,
+			Valid:  true,
+		})
+		if err != nil {
+			return "", errors.WithStack(err)
+		}
 	}
+
+	fmt.Println("User:", user)
 
 	// conversation, err := brain.DatabaseClient.GetConversationByJid(ctx, sql.NullString{
 	// 	String: sender,
@@ -26,10 +44,33 @@ func (brain *BrainEngine) GenerateConversationResponse(ctx context.Context, chan
 	// 	return "", errors.WithStack(err)
 	// }
 
-	// response, err := brain.ProcessMessageResponse(&conversation, message)
-	// if err != nil {
-	// 	return "", errors.WithStack(err)
-	// }
+	lastMessages, err := brain.DatabaseClient.GetMessagesByUserID(ctx, uuid.NullUUID{
+		UUID:  user.ID,
+		Valid: true,
+	})
+	if err != nil {
+		return "", errors.WithStack(err)
+	}
+
+	inputMessage := models.Message{
+		UserID: uuid.NullUUID{
+			UUID:  user.ID,
+			Valid: true,
+		},
+		Content: sql.NullString{
+			String: message,
+			Valid:  true,
+		},
+		Role: sql.NullString{
+			String: openai.ChatMessageRoleUser,
+			Valid:  true,
+		},
+	}
+
+	response, err := brain.ProcessMessageResponse(ctx, &user, lastMessages, &inputMessage)
+	if err != nil {
+		return "", errors.WithStack(err)
+	}
 
 	// brain.DatabaseClient.UpdateConversationBuffer(ctx, models.UpdateConversationBufferParams{
 	// 	ID: conversation.ID,
@@ -39,5 +80,5 @@ func (brain *BrainEngine) GenerateConversationResponse(ctx context.Context, chan
 	// 	},
 	// })
 
-	return "response.PredictedResponse", nil
+	return response.Content.String, nil
 }
--- self/process.go
+++ self/process.go
@@ -1,12 +1,17 @@
 package self
 
 import (
+	"context"
+	"database/sql"
+
+	"github.com/google/uuid"
 	"github.com/minskylab/brain/models"
+	"github.com/pkg/errors"
 	"github.com/sashabaranov/go-openai"
 )
 
-func (brain *BrainEngine) prepareConversationPrompt(user *models.User, lastMessages []*models.Message, message *models.Message) openai.ChatCompletionRequest {
-	messages := make([]openai.ChatCompletionMessage, 0, len(lastMessages)+1)
+func (brain *BrainEngine) prepareMessagesForConversation(user *models.User, lastMessages []models.Message, message *models.Message) []openai.ChatCompletionMessage {
+	messages := []openai.ChatCompletionMessage{}
 
 	messages = append(messages, openai.ChatCompletionMessage{
 		Role:    openai.ChatMessageRoleSystem,
@@ -15,56 +20,24 @@ func (brain *BrainEngine) prepareConversationPrompt(user *models.User, lastMessa
 
 	for _, msg := range lastMessages {
 		role := openai.ChatMessageRoleUser
+		name := user.UserName.String
 
 		if msg.Role.String == openai.ChatMessageRoleAssistant {
 			role = openai.ChatMessageRoleAssistant
+			name = ""
 		}
 
 		messages = append(messages, openai.ChatCompletionMessage{
 			Role:    role,
+			Name:    name,
 			Content: msg.Content.String,
 		})
 	}
 
-	return openai.ChatCompletionRequest{
-		Model:    openai.GPT3Dot5Turbo,
-		Messages: messages,
-	}
+	return messages
 }
 
-func (brain *BrainEngine) Predict(user *models.User, message *models.Message) (string, error) {
-	// prompt := brain.prepareConversationPrompt(conversation, message)
-
-	// ctx := context.Background()
-
-	// fmt.Println("PROMPT:", prompt)
-
-	// engineName := "text-davinci-003"
-
-	// stopSequences := []string{
-	// 	// "\n",
-	// }
-
-	// res, err := brain.LLMEngine.Client.Completion()
-	// res, err := brain.LLMEngine.Client.CompletionWithEngine(ctx, engineName, gpt3.CompletionRequest{
-	// 	Prompt:          []string{prompt},
-	// 	Stop:            stopSequences,
-	// 	N:               gpt3.IntPtr(1),
-	// 	Temperature:     gpt3.Float32Ptr(0.9),
-	// 	TopP:            gpt3.Float32Ptr(1.0),
-	// 	PresencePenalty: 0.6,
-	// 	MaxTokens:       gpt3.IntPtr(1000),
-	// })
-	// if err != nil {
-	// 	return "", errors.WithStack(err)
-	// }
-
-	// return strings.TrimSpace(res.Choices[0].Text), nil
-
-	return "", nil
-}
-
-func (brain *BrainEngine) ProcessMessageResponse(user *models.User, message *models.Message) (*models.Message, error) {
+func (brain *BrainEngine) ProcessMessageResponse(ctx context.Context, user *models.User, lastMessages []models.Message, message *models.Message) (*models.Message, error) {
 	// predicted, err := brain.Predict(conversation, message)
 	// if err != nil {
 	// 	return nil, errors.WithStack(err)
@@ -93,5 +66,34 @@ func (brain *BrainEngine) ProcessMessageResponse(user *models.User, message *mod
 	// 	NewBuffer:         newBuffer,
 	// }, nil
 
-	return nil, nil
+	messages := brain.prepareMessagesForConversation(user, lastMessages, message)
+
+	response, err := brain.LLMEngine.Client.CreateChatCompletion(ctx, openai.ChatCompletionRequest{
+		Model:    openai.GPT3Dot5Turbo,
+		Messages: messages,
+	})
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	responseMessage := models.Message{
+		Role: sql.NullString{
+			String: openai.ChatMessageRoleAssistant,
+			Valid:  true,
+		},
+		Content: sql.NullString{
+			String: response.Choices[0].Message.Content,
+			Valid:  true,
+		},
+		UserID: uuid.NullUUID{
+			UUID:  user.ID,
+			Valid: true,
+		},
+		ParentID: uuid.NullUUID{
+			UUID:  message.ID,
+			Valid: true,
+		},
+	}
+
+	return &responseMessage, nil
 }
