b4c14da1d571c9d04bbb7ba4beeaab12dc4b8e5a
Jhonatan Macazana <46355455+jhonatanmacazana@users.noreply.github.com>
2023-08-11 13:50:20 -0500

    refactor: methods

--- self/process.go
+++ self/process.go
@@ -12,6 +12,26 @@ import (
 	"github.com/sashabaranov/go-openai"
 )
 
+func (brain *BrainEngine) ProcessMessageResponse(ctx context.Context, user models.User, lastMessages []models.Message, inputMessage models.Message) (*openai.ChatCompletionResponse, error) {
+	messages := brain.prepareMessagesForConversation(user, lastMessages, inputMessage)
+
+	fmt.Printf("Total messages: %d\n", len(messages))
+
+	for _, msg := range messages {
+		fmt.Printf("\tMessage: [%s] %s\n", msg.Role, firstN(msg.Content, 100))
+	}
+
+	response, err := brain.LLMEngine.Client.CreateChatCompletion(ctx, openai.ChatCompletionRequest{
+		Model:    openai.GPT3Dot5Turbo,
+		Messages: messages,
+	})
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	return &response, nil
+}
+
 func (brain *BrainEngine) prepareMessagesForConversation(user models.User, lastMessages []models.Message, inputMessage models.Message) []openai.ChatCompletionMessage {
 	messages := []openai.ChatCompletionMessage{}
 
@@ -48,41 +68,43 @@ func (brain *BrainEngine) prepareMessagesForConversation(user models.User, lastM
 	return messages
 }
 
-func (brain *BrainEngine) ProcessMessageResponse(ctx context.Context, user models.User, lastMessages []models.Message, inputMessage models.Message) (*models.Message, error) {
-	messages := brain.prepareMessagesForConversation(user, lastMessages, inputMessage)
-
-	fmt.Printf("Total messages: %d\n", len(messages))
-
-	for _, msg := range messages {
-		fmt.Printf("\tMessage: [%s] %s\n", msg.Role, firstN(msg.Content, 100))
-	}
-
-	response, err := brain.LLMEngine.Client.CreateChatCompletion(ctx, openai.ChatCompletionRequest{
-		Model:    openai.GPT3Dot5Turbo,
-		Messages: messages,
+func (brain *BrainEngine) storeMessage(ctx context.Context, message *models.Message) (models.Message, error) {
+	storedMessage, err := brain.DatabaseClient.CreateMessage(ctx, models.CreateMessageParams{
+		UserID:   message.UserID,
+		Role:     message.Role,
+		Content:  message.Content,
+		ParentID: message.ParentID,
 	})
 	if err != nil {
-		return nil, errors.WithStack(err)
+		return models.Message{}, errors.WithStack(err)
 	}
 
+	fmt.Printf("Stored message [%s][%s]: %s\n", storedMessage.Role.String, storedMessage.UserID.UUID.String(), firstN(storedMessage.Content.String, 100))
+
+	return storedMessage, nil
+}
+
+func buildUserMessage(userId uuid.UUID, messageContent string, lastMessages []models.Message) models.Message {
+	userMessage := models.Message{
+		Role:    sql.NullString{String: openai.ChatMessageRoleUser, Valid: true},
+		UserID:  uuid.NullUUID{UUID: userId, Valid: true},
+		Content: sql.NullString{String: messageContent, Valid: true},
+	}
+
+	if len(lastMessages) > 0 {
+		userMessage.ParentID = uuid.NullUUID{UUID: lastMessages[len(lastMessages)-1].ID, Valid: true}
+	}
+
+	return userMessage
+}
+
+func buildChatbotMessage(userId uuid.UUID, messageContent string, parentId uuid.UUID) models.Message {
 	responseMessage := models.Message{
-		Role: sql.NullString{
-			String: openai.ChatMessageRoleAssistant,
-			Valid:  true,
-		},
-		Content: sql.NullString{
-			String: response.Choices[0].Message.Content,
-			Valid:  true,
-		},
-		UserID: uuid.NullUUID{
-			UUID:  user.ID,
-			Valid: true,
-		},
-		ParentID: uuid.NullUUID{
-			UUID:  inputMessage.ID,
-			Valid: true,
-		},
+		Role:     sql.NullString{String: openai.ChatMessageRoleAssistant, Valid: true},
+		UserID:   uuid.NullUUID{UUID: userId, Valid: true},
+		Content:  sql.NullString{String: messageContent, Valid: true},
+		ParentID: uuid.NullUUID{UUID: parentId, Valid: true},
 	}
 
-	return &responseMessage, nil
+	return responseMessage
 }
